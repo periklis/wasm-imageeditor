ae67641bc90ed0803b94dd7da0419484fa0a84ce
Periklis Tsirakidis <periklis.tsirakidis@mayflower.de>
2017-02-08 20:20:02 +0100

    Add better cmake usage with external projects

---
+++ imageprocessor/test/imageprocessor-test.cpp
@@ -0,0 +1,23 @@
+#include "gtest/gtest.h"
+#include "imageprocessor.hpp"
+
+using namespace mayflower::wasm;
+
+class ImageProcessorFixture : public ::testing::Test {
+ public:
+  ImageProcessorFixture() {
+
+  }
+
+  virtual ~ImageProcessorFixture(){}
+
+  ImageProcessor im {"/data/test.JPEG"};
+
+};
+
+TEST_F(ImageProcessorFixture, ImageProcessorTestDimensions) {
+  auto dims = im.dimensions();
+
+  EXPECT_EQ(440, im.dimensions().find("x")->second);
+  EXPECT_EQ(72, im.dimensions().find("y")->second);
+}
