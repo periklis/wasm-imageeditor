539962165a25567e97e46a133923d7a4870b87df
Periklis Tsirakidis <periklis.tsirakidis@mayflower.de>
2017-02-10 13:42:20 +0100

    Enable ctest based testability on nodejs

---
+++ imageprocessor/src/imageprocessor-bindings.cpp
@@ -0,0 +1,15 @@
+#include "imageprocessor.hpp"
+
+EMSCRIPTEN_BINDINGS(ImageProcessor){
+  emscripten::register_vector<int>("VectorInt");
+  emscripten::register_map<std::string, long>("MapStringLong");
+
+  emscripten::value_object<mayflower::wasm::Dims>("Dimensions")
+      .field("x", &mayflower::wasm::Dims::x)
+      .field("y", &mayflower::wasm::Dims::y);
+
+  emscripten::class_<mayflower::wasm::ImageProcessor>("ImageProcessor")
+      .constructor<std::string>()
+      .function("dimensions", &mayflower::wasm::ImageProcessor::dimensions)
+      .function("histogram", &mayflower::wasm::ImageProcessor::histogram);
+}
--- imageprocessor/src/imageprocessor.hpp
+++ imageprocessor/src/imageprocessor.hpp
@@ -26,18 +26,4 @@ class ImageProcessor {
 }
 }
 
-EMSCRIPTEN_BINDINGS(ImageProcessor){
-  emscripten::register_vector<int>("VectorInt");
-  emscripten::register_map<std::string, long>("MapStringLong");
-
-  emscripten::value_object<mayflower::wasm::Dims>("Dimensions")
-      .field("x", &mayflower::wasm::Dims::x)
-      .field("y", &mayflower::wasm::Dims::y);
-
-  emscripten::class_<mayflower::wasm::ImageProcessor>("ImageProcessor")
-      .constructor<std::string>()
-      .function("dimensions", &mayflower::wasm::ImageProcessor::dimensions)
-      .function("histogram", &mayflower::wasm::ImageProcessor::histogram);
-}
-
 #endif  // IMAGEPROCESSOR_H_
