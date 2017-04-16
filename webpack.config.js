15945c46da2860450c8271cf38178caea0b10373
Periklis Tsirakidis <periklis.tsirakidis@mayflower.de>
2017-04-16 18:56:25 +0200

    Simplify webpack config

---
+++ webpack.config.js
@@ -0,0 +1,254 @@
+const path = require('path');
+const rootDir = path.resolve(__dirname, '.');
+const webpack = require('webpack');
+const webpackMerge = require('webpack-merge');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpack = require('html-webpack-plugin');
+
+const commonConfig = {
+  entry: {
+    vendor: [
+      'react',
+      'react-dom'
+    ]
+  },
+  output: {
+    publicPath: '/',
+    filename: '[name].bundle.js',
+    path: path.resolve(rootDir, 'build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?$/,
+        exclude: /[\/\\]node_modules[\/\\]/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|avg)$/,
+        use: {
+          loader: 'url-loader',
+          query: {
+            limit: 8192,
+            name: 'images/[name].[ext]?[hash]'
+          }
+        }
+      },
+      {
+        test: /\.(woff|woff2|turf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+        use: {
+          loader: 'url-loader',
+          query: {
+            limit: 8192,
+            name: 'fonts/[name].[ext]?[hash]'
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpack({
+      template: path.resolve(rootDir, 'imageeditor', 'components/index.html')
+    })
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx', '.scss'],
+    alias: {
+      Components: path.resolve(rootDir, 'imageeditor/components'),
+      Libs: path.resolve(rootDir, 'imageeditor/libs')
+    }
+  }
+};
+
+const developmentConfig = () => {
+  const config = {
+    devServer: {
+      contentBase: path.resolve(rootDir, 'build'),
+      port: process.env.HOST,
+      host: process.env.PORT
+    },
+    devtool: 'source-map',
+    entry: {
+      application: [
+        'react-hot-loader/patch',
+        './imageeditor/components/index.jsx'
+      ]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          exclude: /[\/\\]node_modules[\/\\]/,
+          use: [
+            {
+              loader: 'style-loader',
+              query: { sourceMap: true }
+            },
+            {
+              loader: 'css-loader',
+              query: {
+                camelCase: true,
+                modules: true,
+                sourceMap: true,
+                importLoaders: 2,
+                localIdentName: '[path]___[name]__[local]___[hash:base64:5]'
+              }
+            },
+            {
+              loader: 'postcss-loader'
+            },
+            {
+              loader: 'sass-loader'
+            }
+          ]
+        },
+        {
+          test: /\.css$/,
+          exclude: /[\/\\]src[\/\\]/,
+          use: [
+            {
+              loader: 'style-loader',
+              query: { sourceMap: true }
+            },
+            {
+              loader: 'css-loader'
+            }
+          ]
+        },
+        {
+          test: /\.css$/,
+          exclude: /[\/\\]node_modules[\/\\]/,
+          use: [
+            {
+              loader: 'style-loader',
+              query: { sourceMap: true }
+            },
+            {
+              loader: 'css-loader',
+              query: {
+                modules: true,
+                sourceMap: true,
+                importLoaders: 1,
+                localIdentName: '[path]___[name]__[local]___[hash:base64:5]'
+              }
+            },
+            {
+              loader: 'postcss-loader'
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  };
+
+  return webpackMerge(commonConfig, config);
+};
+
+const productionConfig = () => {
+  const extractSass = new ExtractTextPlugin({
+    filename: 'application.bundle.css'
+  });
+
+  const config = {
+    devtool: 'cheap-module-source-map',
+    entry: {
+      application: [
+        './imageeditor/components/index.jsx'
+      ]
+    },
+    plugins: [
+      new CopyWebpackPlugin([
+        {
+          from: path.resolve(rootDir, 'imageeditor/images'),
+          to: path.resolve(rootDir, 'build/images')
+        }
+      ]),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          /* eslint-disable camelcase */
+          screw_ie8: true
+          /* eslint-enable camelcase */
+        },
+        output: {
+          comments: false
+        }
+      }),
+      new webpack.LoaderOptionsPlugin({
+        minimize: true,
+        debug: false
+      }),
+      extractSass
+    ],
+    module: {
+      noParse: /\.bundle\.js$/,
+      rules: [
+        {
+          test: /\.scss$/,
+          exclude: /[\/\\]node_modules[\/\\]/,
+          include: [
+            path.resolve(rootDir, 'imageeditor')
+          ],
+          use: extractSass.extract({
+            fallback: 'style-loader',
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  camelCase: true,
+                  import: true,
+                  minimize: true,
+                  modules: true,
+                  sourceMap: false,
+                  importLoaders: 2
+                }
+              },
+              {
+                loader: 'postcss-loader'
+              },
+              {
+                loader: 'sass-loader',
+                options: {
+                  outputStyle: 'compressed',
+                  includePaths: [
+                    path.resolve(rootDir, 'imageeditor/Components')
+                  ]
+                }
+              }
+            ]
+          })
+        },
+        {
+          test: /\.css$/,
+          exclude: /[\/\\]src[\/\\]/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: ['css-loader', 'postcss-loader']
+          })
+        },
+        {
+          test: /\.css$/,
+          exclude: /[\/\\]node_modules[\/\\]/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: ['css-loader', 'postcss-loader']
+          })
+        }
+      ]
+    }
+  };
+
+  return webpackMerge(commonConfig, config);
+};
+
+module.exports = (env) => {
+
+  if (env.target == 'production') {
+    return productionConfig();
+  }
+
+  return developmentConfig();
+};
