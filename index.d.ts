b45bb3f074b8fb94faf33649e5a2ffba8f8e8c3d
Periklis Tsirakidis <periklis.tsirakidis@mayflower.de>
2017-05-01 17:47:42 +0200

    Fix styles and add todos

--- index.d.ts
+++ index.d.ts
@@ -1,85 +1,74 @@
 declare module '*.scss' {
-    const content: any;
-    export default content;
+  const content: any;
+  export default content;
 }
 
+// TODO Replace the following with @types/react-hot-loader
 declare var module: any;
-declare var Module: IEmscriptenModule | any;
-declare var IDBFS: any;
-declare var FS: any;
 
-interface IEmscriptenModule  {
-    arguments: string,
-    environment: string,
-    locateFile: Function,
-    logReadFiles: boolean,
-    noExitRuntime: boolean,
-    noInitialRun: boolean,
-    print: Function,
-    printErr: Function,
-    preInit: Function[],
-    preRun: Function[],
-    wasmBinary: ArrayBuffer
-}
+// TODO Replace the following with a working copy of @types/emscripten
+declare var Module: any;
+declare var FS: any;
+declare var IDBFS: any;
 
 interface Window {
-    Module: IEmscriptenModule | any
+  Module: any;
 }
 
 interface IImageDimensions {
-    width: number,
-    height: number
+  height: number;
+  width: number;
 }
 
 interface IImage {
-    dimensions: IImageDimensions,
-    histogram: number[],
-    imageSrc: string
+  dimensions: IImageDimensions;
+  histogram: number[];
+  imageSrc: string;
 }
 
 interface IToolboxProps {
-    dimensions: IImageDimensions,
-    histogram: number[],
-    onResize(value: IImageDimensions): void,
-    onSave(acceptedFiles: Array<any>): void,
-    onZoom(zoomFactor: number): void
+  dimensions: IImageDimensions;
+  histogram: number[];
+  onResize(value: IImageDimensions): void;
+  onSave(acceptedFiles: Array<any>): void;
+  onZoom(zoomFactor: number): void;
 }
 
 interface ICanvasProps {
-    imageSrc: string
+  imageSrc: string;
 }
 
 interface IAppProps extends IToolboxProps, ICanvasProps {}
 
 interface IImageProcessorProps  {
-    binArguments?: string,
-    environment?: string,
-    locateMemFile?: Function,
-    logReadFiles?: boolean,
-    noExitRuntime?: boolean,
-    noInitialRun?: boolean,
-    print?: Function,
-    printErr?: Function,
-    postRun?: Function[],
-    preInit?: Function[],
-    preRun?: Function[],
-    shellFilename?: string,
-    wasmFilename?: string
+  binArguments?: string[];
+  environment?: string;
+  locateMemFile?: (url: string) => string;
+  logReadFiles?: boolean;
+  noExitRuntime?: boolean;
+  noInitialRun?: boolean;
+  print?: (str: string) => void;
+  printErr?: (str: string) => void;
+  postRun?: (() => void)[];
+  preInit?: (() => void)[];
+  preRun?: (() => void)[];
+  shellFilename?: string;
+  wasmFilename?: string;
 }
 
 declare enum StreamType {STDOUT, STDERR}
 
 interface ILogEntry {
-    text: string,
-    stream: StreamType
+  text: string;
+  stream: StreamType;
 }
 
 interface ILogProps {
-    log?: ILogEntry[]
+  log?: ILogEntry[];
 }
 
 interface IReduxAction {
-    type: string
+  type: string;
 }
 
 interface ILogAction extends IReduxAction, ILogProps {}
